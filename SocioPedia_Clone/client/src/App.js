978e72f5db7ff5493033844913eca32aa5cf3cb6
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-08-12 17:40:18 +0530

    Redux and Toolkit setup; color, theme setup

--- SocioPedia_Clone/client/src/App.js
+++ SocioPedia_Clone/client/src/App.js
@@ -2,18 +2,29 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom' ;
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { CssBaseline , ThemeProvider} from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+
+const mode = useSelector((state) => state.mode);
+const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
 
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage/>}/>
-          <Route path='/home' element={<HomePage/>}/>
-          <Route path='/profile/:userId' element={<ProfilePage/>}/>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path='/' element={<LoginPage/>}/>
+            <Route path='/home' element={<HomePage/>}/>
+            <Route path='/profile/:userId' element={<ProfilePage/>}/>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
       
     </div>
