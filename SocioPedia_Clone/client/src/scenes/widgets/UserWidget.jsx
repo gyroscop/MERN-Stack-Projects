343b11c953a45146ff48dd616be1249ed196e2db
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-08-27 19:30:39 +0530

    UserWidget_1 of homepage code done

--- SocioPedia_Clone/client/src/scenes/widgets/UserWidget.jsx
+++ SocioPedia_Clone/client/src/scenes/widgets/UserWidget.jsx
@@ -9,7 +9,7 @@ import { Box, Typography, Divider, useTheme } from "@mui/material";
 import UserImage from "components/UserImage";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -35,8 +35,116 @@ const UserWidget = ({ userId, picturePath }) => {
   useEffect(() => {
     getUser();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  // when home page loads (components renders) getUser will be called first
 
   if (!user) {
     return null;
   }
+
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedPofile,
+    impressions,
+    friends,
+  } = user;
+
+  return (
+    <WidgetWrapper>
+      {/* FIRST ROW */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap="1rem">
+          <UserImage image={picturePath} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length} friends</Typography>
+          </Box>
+          <ManagedAccountsOutlined />
+        </FlexBetween>
+
+        <Divider />
+        {/* SECOND ROW */}
+        <Box p="1rem 0">
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{location} </Typography>
+          </Box>
+
+          <Box display="flex" alignItems="center" gap="1rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{occupation} </Typography>
+          </Box>
+        </Box>
+
+        {/* THIRD ROW */}
+        <Box p="1rem 0">
+          <FlexBetween>
+            <Typography color={medium}>Who's viewed your profile </Typography>
+            <Typography color={main} fontWeight="500">
+              {viewedPofile}
+            </Typography>
+          </FlexBetween>
+
+          <FlexBetween>
+            <Typography color={medium}>Impressions of your post </Typography>
+            <Typography color={main} fontWeight="500">
+              {impressions}
+            </Typography>
+          </FlexBetween>
+        </Box>
+
+        {/* FOURTH ROW */}
+        <Box p="1rem 0 ">
+          <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+            Social Profiles
+          </Typography>
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="..public/assets/twitter.png" alt="Twitter" />
+              <Box>
+                <Typography color="main" fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color="main">Social Network</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+
+          <FlexBetween gap="1rem">
+            <FlexBetween gap="1rem">
+              <img src="..public/assets/linkedin.png" alt="Twitter" />
+              <Box>
+                <Typography color="main" fontWeight="500">
+                  LinkedIn
+                </Typography>
+                <Typography color="main">Network Platform</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+        </Box>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
 };
+
+export default UserWidget;
